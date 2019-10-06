474d45f109b811162ec9a47a33e1709a45a441ff
Artmen_V <viliutich.artem@gmail.com>
2019-10-06 12:35:23 +0700

    TS_AXIOS_FETCH_DATA_TYPES__hottible_types...

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,41 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { instAPI } from "./api/index";
 import "./App.css";
 
-const App: React.FC = () => {
+export interface DataPost {
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+}
+
+const App: React.FC<any> = () => {
+  const [datas, setDatas] = useState({
+    post: Array<DataPost>()
+  });
+
+  useEffect(() => {
+    const runEffect = async () => {
+      const Chipo = await instAPI({
+        url: `/posts`,
+        method: "get"
+      });
+      setDatas({ post: Chipo.data });
+    };
+    runEffect();
+  }, []);
+
+  console.log(datas, "asdas");
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <header className="App-header">
+        <ul>
+          {datas.post.map(data => {
+            console.log(data.id, "asdas");
+            return <li key={data.id}>{data.title}</li>;
+          })}
+        </ul>
+      </header>
     </div>
   );
 };
---
+++ src/api/index.ts
@@ -0,0 +1,10 @@
+import axios from "axios";
+
+export const instAPI = axios.create({
+  baseURL: "https://jsonplaceholder.typicode.com",
+  headers: {
+    "Content-Type": "application/json;"
+  }
+});
+
+// charset=utf-8
